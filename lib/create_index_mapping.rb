a67eb87b5a90c7393a8b540190e97dd55f2f6b76
Tazro Inutano Ohta <inutano@gmail.com>
2018-08-23 11:09:43 +0900

    modularized

--- lib/create_index_mapping.rb
+++ lib/create_index_mapping.rb
@@ -1,71 +1,79 @@
 require 'json'
 require 'date'
 
-def nested_fields(obj)
-  listing = []
-  obj.each_pair do |key, value|
-    case value
-    when Hash
-      nested_fields(value).each do |child|
-        listing << [key, child]
-      end
-    when Array
-      array_nested(value).each do |item|
-        item.each do |i|
-          listing << [key, i]
+class DateTime
+  class << self
+    def is_date?(string)
+      date = self.parse(string)
+      true if date
+    rescue
+      false
+    end
+  end
+end
+
+module OmicsMetadata
+  class << self
+    def fields(object)
+      evaluate(field_listing(object))
+    end
+
+    def field_listing(object)
+      listing = []
+      object.each_pair do |key, value|
+        case value
+        when Hash
+          field_listing(value).each do |child|
+            listing << [key, child]
+          end
+        when Array
+          array_of_listing(value).each do |item|
+            item.each do |i|
+              listing << [key, i]
+            end
+          end
+        else
+          listing << [key, value]
         end
       end
-    else
-      listing << [key, value]
+      listing
     end
-  end
-  listing
-end
 
-def array_nested(array)
-  array.map do |item|
-    case item
-    when Array
-      array_nested(item)
-    when Hash
-      nested_fields(item)
-    else
-      [item]
+    def array_of_listing(array)
+      array.map do |item|
+        case item
+        when Array
+          array_of_listing(item)
+        when Hash
+          field_listing(item)
+        else
+          [item]
+        end
+      end
     end
-  end
-end
 
-def is_date?(string)
-  date = DateTime.parse(string)
-  true if date
-rescue
-  false
-end
-
-def evaluate_fields(fields)
-  fields.each do |fld|
-    f = fld.flatten
-    value = f.pop
-    tag = f.last
-    if !is_date?(value) && tag !~ /@/
-      p f
+    def evaluate(fields)
+      collection = fields.map do |field|
+        f = field.flatten
+        value = f.pop
+        tag = f.last
+        if !DateTime.is_date?(value) && tag !~ /@/
+          f.join(".")
+        end
+      end
+      collection.compact.uniq
     end
   end
 end
 
 if __FILE__ == $0
-  json_lines = ARGV.first
-  json_array = open(json_lines).readlines.map.with_index(1) do |line, i|
+  json_lines_path = ARGV.first
+
+  json_array = open(json_lines_path).readlines.map.with_index(1) do |line, i|
     if i % 2 == 0
       JSON.load(line)
     end
   end
-  fields = nested_fields(json_array.compact.first)
-  evaluate_fields(fields)
 
-  # objkt = {a: 1, b: [1,2,3], c: {d: 1, e:2, f: {g: 3}}, h: [1,2,3,[{i:4},{j:5}]]}
-  # fields = nested_fields(objkt)
-  # fields.each do |f|
-  #   p f.flatten[0...-1]
-  # end
+  puts OmicsMetadata.fields(json_array.compact.first)
 end
